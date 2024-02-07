c09bbc0d44ff4d2f055e9ac62525992263f64674
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-02-07 11:04:01 -0400

    feat: finish crud

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,21 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import path from "path";
 import dotenv from "dotenv";
-
-
+import tasksRoutes from "./routes";
 
 const app = express();
 
+dotenv.config();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-dotenv.config();
+
+app.use("/api", tasksRoutes);
+
+// read static files
+app.use(express.static(path.join(__dirname, "..", "..", "dist")));
 
 export default app;
+
