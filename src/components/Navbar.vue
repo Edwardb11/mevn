b3b9451e03ceb86408b50438a3808859fb38e025
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-02-07 20:07:25 -0400

    feat: navbar and router link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,20 @@
 
 <template>
-    <div>
-        <h1>Navbar</h1>
-    </div>
+<nav></nav>
+    <ul>
+        <li><router-link to="/">task</router-link></li>
+        <li><router-link to="/task/new">create tasks</router-link></li>
+
+    </ul>
+
 </template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Nav-bar'
+});
+
+</script>
